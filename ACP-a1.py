8ad2ef00f75aa2e647cfcee334df2faafdba6667
ashmialex143-art <ashmialex143@gmail.com>
2026-03-25 16:26:24 +0530

    Add files via upload

---
+++ ACP-a1.py
@@ -0,0 +1,12 @@
+#seasons
+
+season=input("enter season:summer/rainy/winter:")
+
+if season=="summer":
+    print("wear a hat")
+
+elif season=="rainy":
+    print("carry an umberella")
+
+else:
+    print("wear a jacket")
